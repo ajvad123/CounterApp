327c5d05970aef6987f6c98ed9a831dc24d75b8e
Ajvad@123 <mohammadajvad5@gmail.com>
2024-03-18 12:08:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import CounterMain from '../Components/CounterMain'
+
+function App() {
+ 
+
+  return (
+    <>
+
+    <div>
+
+      <CounterMain/>
+
+    </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit"
+import  counterReducer from './slice'
+
+const store=configureStore(
+    {
+        reducer:{
+            counter:counterReducer,
+        }
+    }
+) 
+
+
+export default store
